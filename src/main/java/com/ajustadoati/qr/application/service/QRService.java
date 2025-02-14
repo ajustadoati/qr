c93116ac419314bdff0fdad2c2a7316a581582bc
Richard <richard.rojas@adidas.com>
2025-02-14 19:19:54 +0100

    change qr image

--- src/main/java/com/ajustadoati/qr/application/service/QRService.java
+++ src/main/java/com/ajustadoati/qr/application/service/QRService.java
@@ -36,7 +36,7 @@ public class QRService {
   public ResponseEntity<ByteArrayResource> generateQRCode(String text)
     throws WriterException, IOException {
     QRCodeWriter qrCodeWriter = new QRCodeWriter();
-    BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 300, 300);
+    BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 500, 500);
 
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     MatrixToImageWriter.writeToStream(bitMatrix, "PNG", baos);
